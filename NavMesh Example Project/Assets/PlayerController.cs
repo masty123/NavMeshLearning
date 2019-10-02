f26a05ba18bd62e4149aeb567436e0ee6af7980b
Borisuth Theeruth TIX18 <theeruthborisuth@kamk.fi>
2019-10-02 14:55:59 +0300

    agent can now jump physically

--- NavMesh Example Project/Assets/PlayerController.cs
+++ NavMesh Example Project/Assets/PlayerController.cs
@@ -13,20 +13,43 @@ public class PlayerController : MonoBehaviour
 
     public ThirdPersonCharacter character;
 
+    bool isGrounded;
+
+    public Rigidbody rb;
+
+    public float jumpSpeed;
+
+    RaycastHit hit;
+
+    Ray ray;
+
+    private void Awake()
+    {
+        agent.GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {   
+
+        rb.GetComponent<Rigidbody>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButton(0))
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+           
 
             if(Physics.Raycast(ray, out hit))
             {
+                Debug.Log("" + hit.point);
                 agent.SetDestination(hit.point);
             }
         }
 
+      
         if(agent.remainingDistance > agent.stoppingDistance)
         {
             character.Move(agent.desiredVelocity, false, false);
@@ -35,5 +58,51 @@ public class PlayerController : MonoBehaviour
         {
             character.Move(Vector3.zero, false, false);
         }
+
+      
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (agent.isOnOffMeshLink && isGrounded)
+        {
+            Jump(jumpSpeed);
+            agent.updatePosition = true;
+
+        }
+    }
+
+    private void Jump(float jumpSpeed)
+    {
+       
+            Debug.Log("Jumping/Falling & disabled agent");
+            agent.isStopped = true;
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb.AddRelativeForce(new Vector3(0f, 10f, 2f), ForceMode.Impulse);
+            isGrounded = false;
+            
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            if(!isGrounded)
+            {
+                Debug.Log("Standing & activated agent");
+                isGrounded = true;
+                agent.Warp(transform.position);
+                agent.isStopped = false;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log("" + hit.point);
+                    agent.SetDestination(hit.point);
+                }
+            }
+        }     
     }
 }
