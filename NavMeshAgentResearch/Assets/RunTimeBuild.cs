3e9e781834d91f6b1f5eb21ae5c5654beee094ec
Theeruth Borisuth <theeruth.b@ku.th>
2019-10-03 12:59:38 +0300

    get rid of setDestination error by disable the player before build the navmesh

--- NavMeshAgentResearch/Assets/RunTimeBuild.cs
+++ NavMeshAgentResearch/Assets/RunTimeBuild.cs
@@ -7,10 +7,13 @@ public class RunTimeBuild : MonoBehaviour
 {
     public NavMeshSurface surface;
 
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         surface.BuildNavMesh();
+        player.SetActive(true);
     }
 
     // Update is called once per frame
